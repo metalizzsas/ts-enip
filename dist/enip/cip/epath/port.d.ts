87930ec43b9b74e17cf3567b46913b67607987a5
Kworz <romain.sapet@metalizz.com>
2022-02-11 10:58:43 +0100

    git: removed dist on main branch

--- dist/enip/cip/epath/port.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-declare const PORT_SEGMENT: number;
-declare namespace Epath.port {
-    /** Builds Port Segement for EPATH */
-    function build(port: number, link: number | string): Buffer;
-}
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { ENIP } from './enip';
-export { ENIP };
